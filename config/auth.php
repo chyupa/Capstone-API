ebfd2199171cdcf176b0ca5bfd8ab13bef165c70
Razvan Toader <razvan.toader@gdm.ro>
2016-05-03 21:42:24 +0300

    changed the default user model for authentication.

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\User\Model\User::class,
         ],
 
         // 'users' => [
